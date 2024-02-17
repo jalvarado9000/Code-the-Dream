0f8f12be4444dea15ceabaa3589f3152f497338f
jalvarado9000 <jalvarado9000@gmail.com>
2024-02-17 10:32:46 -0800

    FINISHING THE BOOK LESSON

--- Lesson 1.1 Project Setup And React Basics/hacker-stories/src/App.jsx
+++ Lesson 1.1 Project Setup And React Basics/hacker-stories/src/App.jsx
@@ -2,35 +2,88 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import * as React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+{/* ARRAY OBJECT IN REACT*/}
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <h1>Testing</h1>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+]
+
+{/* ARRAY IN REACT*/}
+
+const welcome = { greeting: 'Hey', title: 'React' }
+const numbers = [1, 2, 3, 4];
+
+{/* MAP IN REACT*/}
+const exponentialNumbers = numbers.map(function (number) {
+  return number * number;
+})
+
+{/* FUNCTION IN REACT*/}
+
+function getTitle(title) {
+  return title;
 }
 
-export default App
+function App() {
+  const title = 'React';
+
+  return (
+
+    <div>
+{/* INPUT AND LABEL*/}
+
+      <h1>My Hacker Stories</h1>
+      <label htmlFor="search">Search: </label>
+      <input type="text" id="search" />
+      <hr />
+
+{/* HOW TO CREATE A LIST ON REACT*/}
+      <ul>
+        {list.map(function (item) {
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title} </a>
+              </span>
+              <span>{item.author} </span>
+              <span>{item.num_comments} </span>
+              <span>{item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
+
+
+
+
+{/* 
+
+      <h1>Hello {title}</h1>
+      <h1>{welcome.greeting} {getTitle(welcome.title)}</h1>
+      <h1>{exponentialNumbers}</h1>
+
+*/}
+
+
+    </div>
+
+  );
+}
+export default App;
